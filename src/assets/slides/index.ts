84d9b667e94c8f7f4a6a0475b0b81fbdeff3d27b
Ascoor <mr.askar1984@gmail.com>
2026-02-06 03:31:07 +0200

    Apply product updates

--- src/assets/slides/index.ts
+++ src/assets/slides/index.ts
@@ -1,8 +1,8 @@
     // Product Images - Centralized exports
     // Frozen products
-    export { default as slide1 } from './slide-1.png';
-    export { default as slide2 } from './slide-2.png';
-    export { default as slide3 } from './slide-3.png';
+    export { default as slide1 } from './slide1.webp';
+    export { default as slide2 } from './slide2.webp';
+    export { default as slide3 } from './slide3.webp';
     export { default as slide4 } from './slide-4.png'; 
     export { default as slide5 } from './slide-5.png'; 
     export { default as slide6 } from './slide-6.png'; 
